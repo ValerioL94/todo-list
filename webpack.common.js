949f3640abe88c017dcefc33c81711b7689fc780
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-04 12:40:08 +0100

    Update repository files

--- webpack.common.js
+++ webpack.common.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: './src/index.js',
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'todo-list',
+            title: 'Todo list',
         }),
     ],
     output: {
