106869c88a8ccfc1f4b474333f29d4bf4c2fc870
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-11 20:01:24 +0100

    Add event listeners on projects' creation

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -4,36 +4,59 @@ const dom = (() => {
     const content = document.getElementById("content");
     const newProjects = document.getElementById("new-projects");
 
-    function displayProject() {
-        projects.projectsList.forEach((el, index) => {
-            const project = document.createElement("div");
-            project.setAttribute("class", "project tab");
-            project.setAttribute("data-id", `${index}`);
-            const projectH3 = document.createElement("h3");
-            projectH3.textContent = `${el.title}`;
-            const btnWrapper = document.createElement("div");
-            const editBtn = document.createElement("button");
-            editBtn.type = "button";
-            const editImg = document.createElement("img");
-            editImg.src = "/images/edit.png";
-            editImg.alt = "pen and paper icon";
-            editImg.title = "Edit";
-            const deleteBtn = document.createElement("button");
-            deleteBtn.type = "button";
-            const deleteImg = document.createElement("img");
-            deleteImg.src = "/images/delete.png";
-            deleteImg.alt = "trash bin icon"
-            deleteImg.title = "Delete";
+    function displayProject(project) {
+        const newProject = document.createElement("div");
+        newProject.setAttribute("class", "project tab");
+        newProject.setAttribute("data-id", `${project.id}`);
+        newProject.setAttribute("tabindex", "0");
+        newProject.addEventListener("click", () => {
+            const selectedEl = document.querySelector(".selected");
+            if (selectedEl) selectedEl.classList.remove("selected");
+            newProject.classList.add("selected");
+        });
+        const projectH3 = document.createElement("h3");
+        projectH3.textContent = `${project.title}`;
+        const btnWrapper = document.createElement("div");
+        const editBtn = document.createElement("button");
+        editBtn.type = "button";
+        editBtn.setAttribute("class", "editProject");
+        // editBtn.addEventListener("click", () => {
 
-            newProjects.appendChild(project);
-            project.append(projectH3, btnWrapper);
-            btnWrapper.append(editBtn, deleteBtn);
-            editBtn.appendChild(editImg);
-            deleteBtn.appendChild(deleteImg);
+        // })
+        const editImg = document.createElement("img");
+        editImg.src = "/images/edit.png";
+        editImg.alt = "pen and paper icon";
+        editImg.title = "Edit";
+        const deleteBtn = document.createElement("button");
+        deleteBtn.type = "button";
+        deleteBtn.setAttribute("class", "deleteProject");
+        deleteBtn.addEventListener("click", () => {
+            if (confirm("Delete this project?") === true) {
+                projects.deleteProject(`${project.id}`);
+                newProject.remove();
+            }
         })
+        const deleteImg = document.createElement("img");
+        deleteImg.src = "/images/delete.png";
+        deleteImg.alt = "trash bin icon"
+        deleteImg.title = "Delete";
+
+        newProjects.appendChild(newProject);
+        newProject.append(projectH3, btnWrapper);
+        btnWrapper.append(editBtn, deleteBtn);
+        editBtn.appendChild(editImg);
+        deleteBtn.appendChild(deleteImg);
+
+    }
+
+    function displayProjectsList() {
+        for (let i = 0; i < projects.projectsList.length; i++) {
+            displayProject(projects.projectsList[i]);
+        }
     }
     return {
         displayProject,
+        displayProjectsList,
     }
 })()
 
