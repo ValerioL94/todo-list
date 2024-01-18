8fbb7118aaef1b314b138c246dd5bde1a970b488
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-18 14:00:57 +0100

    Add event listeners and task modal options (WiP)

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -34,7 +34,7 @@ const dom = (() => {
         const projectCancel = document.getElementById("projectCancel");
 
         let currentProject;
-        let currentH3;
+        let currentProjectH3;
 
         newProjectBtn.addEventListener("click", () => {
             projectModal("add");
@@ -44,13 +44,13 @@ const dom = (() => {
             const button = event.target.closest("button");
             if (!button) return;
             currentProject = event.target.closest(".project.tab");
-            currentH3 = currentProject.querySelector("h3");
+            currentProjectH3 = currentProject.querySelector("h3");
             if (button.className === "editProject") {
                 projectModal("edit");
             } else if (button.className === "deleteProject") {
                 projectModal("delete");
             }
-            projectTitle.value = currentH3.textContent;
+            projectTitle.value = currentProjectH3.textContent;
             projectDialog.showModal();
         })
 
@@ -94,7 +94,7 @@ const dom = (() => {
         };
         function editProject() {
             const newTitle = projectTitle.value;
-            const oldTitle = currentH3.textContent;
+            const oldTitle = currentProjectH3.textContent;
             if (newTitle === "") alert("Please enter new title");
             else if (projects.projectsList.some(element => element.title === newTitle)) {
                 alert("Project already exists!");
@@ -102,7 +102,7 @@ const dom = (() => {
                 projects.editProject(newTitle, oldTitle);
                 projectDialog.close();
                 projectForm.reset();
-                currentH3.textContent = newTitle;
+                currentProjectH3.textContent = newTitle;
             }
         }
         function deleteProject() {
@@ -201,17 +201,18 @@ const dom = (() => {
     })
 
     let currentProject;
-    let currentH3;
+    let currentProjectH3;
 
     newProjects.addEventListener("click", (event) => {
         const projectTab = event.target.closest(".project.tab");
         if (!projectTab) return;
-        currentProject = event.target.closest(".project.tab");
-        currentH3 = currentProject.querySelector("h3");
         const projectTabTitle = projectTab.querySelector("h3");
+        currentProject = event.target.closest(".project.tab");
+        currentProjectH3 = currentProject.querySelector("h3");
         updateTabTitle(projectTabTitle.textContent);
         updateTabIcon("images/project.png", "pan and paper icon");
         tasksCount(projectTabTitle.textContent);
+
     })
 
     function updateTabTitle(newTitle) {
@@ -231,6 +232,7 @@ const dom = (() => {
     }
 
     const newTaskBtn = document.getElementById("newTaskBtn");
+    const newTasks = document.getElementById("newTasks");
     const taskDialog = document.getElementById("taskDialog");
     const taskConfirm = document.getElementById("taskConfirm");
     const taskCancel = document.getElementById("taskCancel");
@@ -260,28 +262,68 @@ const dom = (() => {
         taskDialog.close();
         taskForm.reset();
     });
+
     function createTask() {
         const title = taskTitle.value;
         const description = taskDescription.value;
         const date = taskDate.value;
         const priority = taskPriority.value;
-        let index = projects.projectsList.findIndex(el => el.title === currentH3.textContent);
+        const projectH3 = currentProjectH3.textContent;
+        let index = projects.projectsList.findIndex(el => el.title === projectH3);
         if (title === "") alert("Please enter a title");
-        else if (projects.projectsList[index].tasks.some(element => element.title === title)) {
+        else if (projects.projectsList[index].tasks.some(el => el.title === title)) {
             alert("Project already exists!");
         } else {
             tasks.createTask(title, description, date, priority, index);
             displayTask(title, date, priority);
-            tasksCount(currentH3.textContent);
+            tasksCount(projectH3);
             taskDialog.close();
             taskForm.reset();
         }
     }
 
-    const newTasks = document.getElementById("newTasks");
+    let currentTask;
+    let currentTaskH3;
+
+    newTasks.addEventListener("click", (event) => {
+        const taskTab = event.target.closest(".task");
+        if (!taskTab) return;
+        const taskH3 = taskTab.querySelector("h3").textContent;
+        const tabSpan = taskTab.querySelector("span");
+        currentTask = event.target.closest(".task");
+        currentTaskH3 = currentTask.querySelector("h3").textContent;
+        const projectH3 = currentProjectH3.textContent;
+        tabSpan.classList.toggle("completed");
+        tasks.taskCompleted(taskH3, projectH3);
+    })
+
+    function taskModal(value) {
+        switch (value) {
+            case "add":
+                dialogTitle.textContent = "New Project";
+                projectTitle.disabled = false;
+                projectConfirm.className = "addBtn";
+                projectConfirm.textContent = "Add";
+                break;
+            case "edit":
+                dialogTitle.textContent = "Edit Project";
+                projectTitle.disabled = false;
+                projectConfirm.className = "editBtn";
+                projectConfirm.textContent = "Edit";
+                break;
+            case "delete":
+                dialogTitle.textContent = "Delete Project";
+                projectTitle.disabled = true;
+                projectConfirm.className = "deleteBtn";
+                projectConfirm.textContent = "Delete";
+                break;
+        }
+    }
+
     function displayTask(title, date, priority) {
         const task = document.createElement("div");
         task.setAttribute("class", `task priority${priority}`);
+        const taskSpan = document.createElement("span");
         const titleWrapper = document.createElement("div");
         titleWrapper.setAttribute("class", "titleWrapper");
         const taskH3 = document.createElement("h3");
@@ -308,7 +350,7 @@ const dom = (() => {
         deleteImg.alt = "trash bin icon";
 
         newTasks.appendChild(task);
-        task.append(titleWrapper, taskDetails);
+        task.append(taskSpan, titleWrapper, taskDetails);
         titleWrapper.appendChild(taskH3);
         taskDetails.append(taskDate, taskButtons);
         taskButtons.append(taskInfo, taskEdit, taskDelete);
