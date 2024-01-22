274511213ee24ad6d8ec211e335cee2b656db4b9
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-22 13:08:57 +0100

    Update functions, add test tasks

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -3,55 +3,58 @@ import projects from "./projects";
 const tasks = (() => {
 
     class Task {
-        constructor(title, description, dueDate, priority) {
+        constructor(title, description, dueDate, priority, project) {
             this.title = title
             this.description = description
             this.dueDate = dueDate
             this.priority = priority
-            this.completed = false
+            this.project = project
+            this.completed = "No"
         }
     }
 
-    const test1 = new Task("taskTest1", "this is a test", "2024-01-21", "Low");
-    const test2 = new Task("taskTest2", "another test", "2024-01-20", "Medium");
-    const test3 = new Task("taskTest3", "one more test", "2024-01-19", "High");
-    const test4 = new Task("taskTest4", "one last test", "2024-01-18", "Low");
+    const test1 = new Task("Buy healthy food", "Stop eating snacks and pre-packaged stuff, cook your own meals and buy fresh vegetables ", "2024-01-28", "High", "Eat healthy");
+    const test2 = new Task("Drink more water", "Drink more water, 3 liters of water each day may seem a lot but it's not!", "2024-01-25", "Low", "Stay hydrated");
+    const test3 = new Task("Start training", "The hardest part is making a habit of it, you just need to get the ball rolling", "2024-01-22", "Medium", "Training");
+    const test4 = new Task("Have some fun", "Study hard, party hard. Studying and/or working is as important as having fun.", "2024-01-20", "Medium", "Have fun");
 
     function test() {
-        pushTask(0, test1);
-        pushTask(1, test2);
-        pushTask(2, test3);
-        pushTask(3, test4);
+        pushTask(test1, 0);
+        pushTask(test2, 1);
+        pushTask(test3, 2);
+        pushTask(test4, 3);
     }
-    function createTask(title, description, dueDate, priority, projectIndex) {
-        let task = new Task(title, description, dueDate, priority);
-        pushTask(projectIndex, task);
+    function createTask(title, description, dueDate, priority, project) {
+        let task = new Task(title, description, dueDate, priority, project);
+        let index = projects.getIndex(project);
+        pushTask(task, index);
     }
-    function pushTask(index, task) {
+    function pushTask(task, index) {
         projects.projectsList[index].tasks.push(task);
     }
-    function editTask(title, description, dueDate, priority, projectTitle) {
-        deleteTask(projectTitle);
-        createTask(title, description, dueDate, priority, projectTitle);
+    function editTask(title, description, dueDate, priority, project, taskIndex, projectIndex) {
+        let task = new Task(title, description, dueDate, priority, project);
+        replaceTask(taskIndex, projectIndex, task);
     }
-    function deleteTask(task, project) {
-        let projectIndex = projects.getIndex(project);
-        let taskIndex = getIndex(projectIndex, task);
+    function deleteTask(taskIndex, projectIndex) {
         projects.projectsList[projectIndex].tasks.splice(taskIndex, 1);
+    }
+    function replaceTask(taskIndex, projectIndex, task) {
+        projects.projectsList[projectIndex].tasks.splice(taskIndex, 1, task);
     }
 
     function taskCompleted(task, project) {
         let projectIndex = projects.getIndex(project);
         let taskIndex = getIndex(projectIndex, task);
-        if (projects.projectsList[projectIndex].tasks[taskIndex].completed === false) {
-            projects.projectsList[projectIndex].tasks[taskIndex].completed = true;
+        if (projects.projectsList[projectIndex].tasks[taskIndex].completed === "No") {
+            projects.projectsList[projectIndex].tasks[taskIndex].completed = "Yes";
         } else {
-            projects.projectsList[projectIndex].tasks[taskIndex].completed = false;
+            projects.projectsList[projectIndex].tasks[taskIndex].completed = "No";
         }
     }
-    function getIndex(projectIndex, title) {
-        let taskIndex = projects.projectsList[projectIndex].tasks.findIndex(el => el.title === title);
-        return taskIndex;
+    function getIndex(projectIndex, taskTitle) {
+        let index = projects.projectsList[projectIndex].tasks.findIndex(el => el.title === taskTitle);
+        return index;
     }
 
     return {
@@ -59,6 +62,7 @@ const tasks = (() => {
         editTask,
         deleteTask,
         taskCompleted,
+        getIndex,
         test,
     }
 })()
