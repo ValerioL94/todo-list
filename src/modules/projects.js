2dd50bd9633db3d2113067c0c468b109618182ee
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-23 01:22:39 +0100

    Add default projects if local storage is empty

--- src/modules/projects.js
+++ src/modules/projects.js
@@ -5,12 +5,67 @@ const projects = (() => {
             this.tasks = [];
         }
     }
-    const test1 = new Project("Eat healthy");
-    const test2 = new Project("Stay hydrated");
-    const test3 = new Project("Training");
-    const test4 = new Project("Have fun");
 
-    const projectsList = [test1, test2, test3, test4];
+    let projectsList = [];
+    if (localStorage.getItem('projects') === null) {
+        projectsList = [
+            {
+                title: "Eat healthy",
+                tasks: [
+                    {
+                        title: "Buy healthy food",
+                        description: "Stop eating snacks and pre-packaged stuff, cook your own meals and buy fresh vegetables ",
+                        dueDate: "2024-01-28",
+                        priority: "High",
+                        project: "Eat healthy",
+                        completed: "No",
+                    }
+                ]
+            },
+            {
+                title: "Stay hydrated",
+                tasks: [
+                    {
+                        title: "Drink more water",
+                        description: "3 liters of water each day may seem a lot but it's not!",
+                        dueDate: "2024-01-25",
+                        priority: "Low",
+                        project: "Stay hydrated",
+                        completed: "No",
+                    }
+                ]
+            },
+            {
+                title: "Training",
+                tasks: [
+                    {
+                        title: "Start training",
+                        description: "The hardest part is making a habit of it, you just need to get the ball rolling",
+                        dueDate: "2024-01-22",
+                        priority: "Medium",
+                        project: "Training",
+                        completed: "No",
+                    }
+                ]
+            },
+            {
+                title: "Have fun",
+                tasks: [
+                    {
+                        title: "Do something you enjoy",
+                        description: "Study hard, party hard. Studying and/or working is as important as having fun.",
+                        dueDate: "2024-01-20",
+                        priority: "Medium",
+                        project: "Have fun",
+                        completed: "No",
+                    }
+                ]
+            }
+        ]
+    } else {
+        const storedProjects = JSON.parse(localStorage.getItem('projects'));
+        projectsList = storedProjects;
+    }
 
     function createProject(title) {
         let project = new Project(title);
