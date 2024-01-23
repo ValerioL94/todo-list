d292472a1c1e30149b1177f6c85d115da372cbae
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-01-23 01:20:51 +0100

    remove test function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import dom from './modules/dom.js';
-import tasks from './modules/tasks.js';
 import './styles.css';
 
 function importAll(r) {
@@ -9,5 +8,4 @@ importAll(require.context('/src/images', false, /\.(png|svg|jpg|jpeg|gif)$/));
 
 dom.initPage();
 dom.displayProjectsList();
-tasks.test();
 dom.allTab.click();
